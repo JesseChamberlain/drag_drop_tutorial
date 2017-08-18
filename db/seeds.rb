ffb959f983c710092d57d0ec2b252fb5f6cbef92
JesseChamberlain <chamberlain.jesse@gmail.com>
2017-08-18 15:12:21 -0400

    Create Blocks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,61 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+BLOCKS = [
+  {
+    name: "I",
+    color: "green",
+    location: "1"
+  },
+  {
+    name: "A1",
+    color: "red",
+    location: "2"
+  },
+  {
+    name: "A2",
+    color: "red",
+    location: "3"
+  },
+  {
+    name: "A3",
+    color: "red",
+    location: "4"
+  },
+  {
+    name: "B1",
+    color: "pink",
+    location: "5"
+  },
+  {
+    name: "B2",
+    color: "pink",
+    location: "6"
+  },
+  {
+    name: "BB",
+    color: "pink",
+    location: "7"
+  },
+  {
+    name: "C",
+    color: "green",
+    location: "8"
+  },
+  {
+    name: "A4",
+    color: "red",
+    location: "9"
+  }
+].freeze
+
+BLOCKS.each do |b|
+  block = Block.new(
+    name: b[:name],
+    color: b[:color],
+    location: b[:location]
+  )
+  block.assign_attributes(b)
+  block.save!
+end
