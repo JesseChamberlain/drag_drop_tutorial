0d9ea03970e905dde262eaa434cd6645980804a5
JesseChamberlain <chamberlain.jesse@gmail.com>
2017-08-23 14:37:01 -0400

    Setup basic layout

--- app/controllers/api/v1/blocks_controller.rb
+++ app/controllers/api/v1/blocks_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::BlocksController < ApplicationController
-  
+
+  # Assigns a variable an array of all the blocks in the database
+  # It then sorts the blocks based on the location column and returns as a JSON.
   def index
-    render json: Block.all
+    blocks = Block.all
+    blocks = blocks.order(location: :asc)
+    render json: blocks
   end
 end
