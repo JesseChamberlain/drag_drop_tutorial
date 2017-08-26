6c4c94609ad3034fd3b0ba96a0bedba49b50bc0e
JesseChamberlain <chamberlain.jesse@gmail.com>
2017-08-26 11:50:28 -0400

    Fetch successfully hitting back end

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ListsController < ApplicationController
-
+  skip_before_action :verify_authenticity_token
+  
   def index
     render json List.all
   end
@@ -11,5 +12,27 @@ class Api::V1::ListsController < ApplicationController
     blocks = list.blocks
     blocks = blocks.order(location: :asc)
     render json: {list: list, blocks: blocks}
+  end
+
+  def update
+    binding.pry
+    data = JSON.parse(request.body.read)
+
+    # blocks = Song.find(params[:id]).blocks
+    # blocks.each do |block|
+    #   data["blocks"].each_with_index do |d, i|
+    #     if d["id"] == block.id
+    #       new_location = (i + 1)
+    #       unless new_location == block.location
+    #         block.location = new_location
+    #         block.save
+    #       end
+    #     end
+    #   end
+    # end
+    # song = Song.find(params[:id])
+    # sorted_blocks = blocks.sort_by{|b| b["location"]}
+    # render json: {song: song, blocks: sorted_blocks}
+
   end
 end
--- react/src/containers/BlocksContainer.js
+++ react/src/containers/BlocksContainer.js
@@ -11,6 +11,7 @@ class BlocksContainer extends Component {
       blocks: []
     }
     this.onSortEnd = this.onSortEnd.bind(this);
+    this.updateListBlocks = this.updateListBlocks.bind(this);
   }
 
   // Sets the state with the blocks within the database
@@ -34,10 +35,34 @@ class BlocksContainer extends Component {
     .catch(error => console.error(`Error in fetch: ${error.message}`))
   }
 
+  // Send resorted array PATCH as JSON
+  updateListBlocks(blocks) {
+    let data = {blocks: blocks};
+    let jsonStringData = JSON.stringify(data);
+    let id = this.state.list.id
+    fetch(`/api/v1/lists/${id}`, {
+      method: 'PATCH',
+      body: jsonStringData
+    })
+    .then(response => {
+      if (response.ok) {
+        return response;
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+            error = new Error(errorMessage);
+        throw(error);
+      }
+    })
+    .then(response => response.json())
+    .catch(error => console.error(`Error in fetch: ${error.message}`));
+  }
+
+  // Re-sorts the array after dragging.
   onSortEnd({oldIndex, newIndex}) {
     this.setState({
       blocks: arrayMove(this.state.blocks, oldIndex, newIndex),
     });
+    this.updateListBlocks(this.state.blocks)
   };
 
   render() {
