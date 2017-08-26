f8da9df406471d783152a4ef09a78640b1f570e1
JesseChamberlain <chamberlain.jesse@gmail.com>
2017-08-26 13:25:15 -0400

    sort persisting in the database

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ListsController < ApplicationController
   skip_before_action :verify_authenticity_token
-  
+
   def index
     render json List.all
   end
@@ -15,24 +15,22 @@ class Api::V1::ListsController < ApplicationController
   end
 
   def update
-    binding.pry
     data = JSON.parse(request.body.read)
+    blocks = List.find(params[:id]).blocks
+    blocks.each do |block|
+      data["blocks"].each_with_index do |d, i|
+        if d["id"] == block.id
+          new_location = (i + 1)
+          unless new_location == block.location
+            block.location = new_location
+            block.save
+          end
+        end
+      end
+    end
 
-    # blocks = Song.find(params[:id]).blocks
-    # blocks.each do |block|
-    #   data["blocks"].each_with_index do |d, i|
-    #     if d["id"] == block.id
-    #       new_location = (i + 1)
-    #       unless new_location == block.location
-    #         block.location = new_location
-    #         block.save
-    #       end
-    #     end
-    #   end
-    # end
-    # song = Song.find(params[:id])
-    # sorted_blocks = blocks.sort_by{|b| b["location"]}
-    # render json: {song: song, blocks: sorted_blocks}
-
+    list = List.find(params[:id])
+    blocks = blocks.order(location: :asc)
+    render json: {list: list, blocks: blocks}
   end
 end
